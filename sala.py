8f43616918bb1664da33b19423cf833f453044d3
DanielaO85 <daniela.da.silva@gmail.com>
2023-04-16 17:58:04 -0300

    incluindo metodos

--- sala.py
+++ sala.py
@@ -1,1 +1,16 @@
-hh
+class Sala{
+    def __init__(num_sala, capacidade, assentos, ocupacao):
+        self._num_sala = num_sala
+        self._capacidade = capacidade
+        self._assentos = assentos
+        self._ocupacao = ocupacao
+        
+        def alocacao_vendas(self, assentos, ocupacao)    
+            if self._ocupacao = True:
+            return f'Assento {self._assento} Vendido!'
+            else
+            f'Assento {self._assento} Disponivel.'
+        
+        del capacidade_salas(self, num_sala, capacidade)
+            return f'A {self._num_sala} tem capacidade de {self._capacidade}'       
+}

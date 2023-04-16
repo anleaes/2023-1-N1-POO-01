ab9e357f6fb8cfafbb38c6b740bfe333a8d343d8
DanielaO85 <daniela.da.silva@gmail.com>
2023-04-16 18:20:36 -0300

    esboço metodos

--- sala.py
+++ sala.py
@@ -1,16 +1,17 @@
-class Sala{
-    def __init__(num_sala, capacidade, assentos, ocupacao):
+class Sala:
+    
+    def __init__(self, num_sala, capacidade, assentos, ocupacao):
         self._num_sala = num_sala
         self._capacidade = capacidade
         self._assentos = assentos
         self._ocupacao = ocupacao
         
-        def alocacao_vendas(self, assentos, ocupacao)    
-            if self._ocupacao = True:
-            return f'Assento {self._assento} Vendido!'
-            else
-            f'Assento {self._assento} Disponivel.'
+        def alocacao_vendas(self, assentos, ocupacao):    
+            if self._ocupacao:
+             return f'Assento {self._assento} Vendido!'
+            else:
+             return f'Assento {self._assento} Disponivel.'
         
-        def capacidade_salas(self, num_sala, capacidade)
+        def capacidade_salas(self, num_sala, capacidade):
             return f'A {self._num_sala} tem capacidade de {self._capacidade}'       
-}
+
--- sessao.py
+++ sessao.py
@@ -1,18 +1,18 @@
 from sala import Sala
-class Sessao{
-    def __init__(data_sessao, hora_sessao, encerrada, num_sala, assentos, ocupacao):
+
+class Sessao:
+    def __init__(self, data_sessao, hora_sessao, encerrada, num_sala, assentos, ocupacao):
        super().__init__(num_sala, assentos, ocupacao)
        self._num_sala = data_sessao
        self._assentos = hora_sessao
        self._ocupacao = encerrada
       
-      def sala_sessao(self, data_sessao, hora_sessao, num_sala) 
-          return f'A sala {self._num_sala} tem sessao em {self._data_sessao} as {self._hora_sessao}
+       def sala_sessao(self, data_sessao, hora_sessao, num_sala): 
+           return f'A sala {self._num_sala} tem sessao em {self._data_sessao} as {self._hora_sessao}'
       
-      def situacao_sessao(encerrada, data_sessao, hora_sessao, num_sala)   
-          return f'A disponibilidade da sessao da {self._num_sala} em {self._data_sessao} as {self.hora_sessao} esta'
-           if encerrada = True:
-            return f' encerrada!'
-            else
-            f' com vendas abertas.'
-}
+       def situacao_sessao(self, encerrada, data_sessao, hora_sessao, num_sala):   
+           print('A disponibilidade da sessao da {self._num_sala} em {self._data_sessao} as {self.hora_sessao} esta')
+           if encerrada:
+              return f' encerrada!'
+           else:
+             return f' com vendas abertas.'

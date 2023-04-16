a9953dc286a332c34bd198b636d28c340966d1ce
FerCarvalhoRP <fernandacarvalhorp@gmail.com>
2023-04-16 20:44:13 -0300

    Atualizando arquivos

---
+++ cinema.py
@@ -0,0 +1,10 @@
+class Cinema:
+    
+    def __init__(razao_social, responsavel, cnpj, endereco, telefone, cep, email):
+        self._razao_social = razao_social 
+        self._responsavel = responsavel
+        self._cnpj = cnpj
+        self._endereco = endereco
+        self._telefone = telefone
+        self._cep = cep
+        self._email = email

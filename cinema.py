34b0c059534615fd2015ce6ef76ef6ac1e69a92c
FerCarvalhoRP <fernandacarvalhorp@gmail.com>
2023-04-16 22:04:23 -0300

    atulizando arquivos

--- cinema.py
+++ cinema.py
@@ -1,10 +1,13 @@
 class Cinema:
-    
-    def __init__(razao_social, responsavel, cnpj, endereco, telefone, cep, email):
-        self._razao_social = razao_social 
+
+    def __init__(self, id, responsavel, cnpj, endereco, telefone, cep, email):
+        self._id = id
         self._responsavel = responsavel
         self._cnpj = cnpj
         self._endereco = endereco
         self._telefone = telefone
         self._cep = cep
         self._email = email
+        
+    def escolher_cinema (self):
+        return self._cep._endereco

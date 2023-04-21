b35ca89b790e04b23f495ae6405bb3be16c8be11
eduardalutz <eduarda.lutz.silva@gmail.com>
2023-04-21 14:05:50 -0300

    atualização codigo filme

--- filme.py
+++ filme.py
@@ -1,11 +1,9 @@
-class filme:
-    
-    def __init__(self, titulo, genero, duracao, clasindicativa):
-        
-        self._titulo = titulo
-        self._genero = genero
-        self._duracao = duracao
-        self._clasindicativa = clasindicativa
+class Filme:
+        def __init__(self, nome: str, duracao: str, genero: str, classificacao: str):
+        self.nome = nome
+        self.duracao = duracao
+        self.genero = genero
+        self.classificacao = classificacao
         
     def get_filme(self):
-        return self.titulo + " - " + self.genero + " - " + self.duracao + " - " + self.clasindicativa
+        return self.nome + " - " + self.genero + " - " + self.duracao + " - " + self.classificacao

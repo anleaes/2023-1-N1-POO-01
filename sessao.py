e729a76fd57fdb91ac8d60ec79cf8b2a73e77962
DanielaO85 <daniela.da.silva@gmail.com>
2023-04-21 12:17:00 -0300

    um ajuste

--- sessao.py
+++ sessao.py
@@ -7,14 +7,16 @@ class Sessao:
        self._hora_sessao = hora_sessao       
       
        def sala_sessao(self, data_sessao):
+           
            if data_sessao == self._data_sessao: 
             return f'A sala {self._num_sala} tem sessao em {self._data_sessao} as {self._hora_sessao}'
            else:
              return f'Nao ha sessao na data escolhida.'  
           
        def situacao_sessao(self, data_sessao):  
-           if data_sessao == self._data_sessao: 
-               if self._ocupacao == 1: 
-                return f'A disponibilidade da sessao da {self._num_sala} em {self._data_sessao} as {self.hora_sessao} esta encerrada!'
-               else:
-                return f'Segue com vendas abertas {self._num_sala} em {self._data_sessao} as {self.hora_sessao}.'
+           for self in self.assentos:
+            if data_sessao == self._data_sessao: 
+                if self._assentos == [True]: 
+                 return f'A disponibilidade da sessao da {self._num_sala} em {self._data_sessao} as {self.hora_sessao} esta encerrada!'
+                else:
+                 return f'Segue com vendas abertas {self._num_sala} em {self._data_sessao} as {self.hora_sessao}.'

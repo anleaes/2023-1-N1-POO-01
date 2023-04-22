595ee96af4361b9d71fbb074480c8a495a69291c
mirianns <130416417+mirianns@users.noreply.github.com>
2023-04-22 15:52:48 -0300

    ingresso

---
+++ cliente.py
@@ -0,0 +1,14 @@
+class Cliente:
+    def __init__(self, nome, cpf, data_nascimento, telefone, email, tipo, id, preco_ingresso, itens, preco, nota_fiscal):
+       super().__init__(tipo, id, preco_ingresso, itens, preco, nota_fiscal)
+       self._nome = nome
+       self._cpf = cpf
+       self._data_nascimento = data_nascimento
+       self._telefone = telefone
+       self._email = email
+           
+    def contato_cliente(self, nome, telefone, email):
+        set(self._nome, self._telefone, self._email)
+        
+    def consumo_cliente(self, nome, email, ingresso, compras):
+        return self._nome, self._email, self._ingresso, self._compras

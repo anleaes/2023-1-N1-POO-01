56927ee8d6c982940b02487f8295b39b59092d81
eduardalutz <eduarda.lutz.silva@gmail.com>
2023-04-22 14:34:34 -0300

    atualizacao loja

--- loja.py
+++ loja.py
@@ -1,7 +1,17 @@
 class Loja:
-
-  def __init__(itens,preco, nota_fiscal):
+    def __init__(self, nota_fiscal, itens, preco):
     self.itens = itens
     self.preco = preco
-    self.nf = nf
+    self.nota_fiscal = nota_fiscal
 
+    def compra(self, nota_fiscal, itens, preco):
+    self.itens = itens
+    self.preco = preco
+    self.nota_fiscal = nota_fiscal
+    return f"Compra realizada com sucesso. Nota Fiscal: {self.nota_fiscal}"
+
+    def tabela_precos(self, itens, preco):
+    tabela = {}
+    for i in range(len(itens)):
+        tabela[itens[i]] = preco[i]
+    return tabela

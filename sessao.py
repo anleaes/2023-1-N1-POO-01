4e293d78f53664846ff96c09bf1b218ff36cc170
DanielaO85 <daniela.da.silva@gmail.com>
2023-04-23 19:52:45 -0300

    correçoes após rodar

--- sessao.py
+++ sessao.py
@@ -1,22 +1,22 @@
 from sala import Sala
 
 class Sessao:
-    def __init__(self, data_sessao, hora_sessao, num_sala, assentos):
-       super().__init__(num_sala, assentos)
+     def __init__(self, data_sessao, hora_sessao, sala): 
        self._data_sessao = data_sessao
-       self._hora_sessao = hora_sessao       
+       self._hora_sessao = hora_sessao
+       self._sala = sala       
       
        def sala_sessao(self, data_sessao):
            
            if data_sessao == self._data_sessao: 
-            return f'A sala {self._num_sala} tem sessao em {self._data_sessao} as {self._hora_sessao}'
+            return f'A sala {self._sala._num_sala} tem sessao em {self._data_sessao} as {self._hora_sessao}'
            else:
-             return f'Nao ha sessao na data escolhida.'  
+            return f'Nao ha sessao na data escolhida.'  
           
        def situacao_sessao(self, data_sessao):  
-           for self in self.assentos:
-            if data_sessao == self._data_sessao: 
-                if self._assentos == [True]: 
-                 return f'A disponibilidade da sessao da {self._num_sala} em {self._data_sessao} as {self.hora_sessao} esta encerrada!'
-                else:
-                 return f'Segue com vendas abertas {self._num_sala} em {self._data_sessao} as {self.hora_sessao}.'
+           if data_sessao != self._data_sessao: 
+            return f'Não há sessão nesta data.\n'
+           for i in range(len(self._sala._assentos)):    
+            if self._sala._assentos[i] == False: 
+              return f'Segue com vendas abertas {self._sala._num_sala} em {self._data_sessao} as {self._hora_sessao}.'      
+           return f'A disponibilidade da sessao da {self._sala._num_sala} em {self._data_sessao} as {self._hora_sessao} esta encerrada!'

3b70ae332fbc5e2c7236d47df07f78f9439ccd53
FerCarvalhoRP <fernandacarvalhorp@gmail.com>
2023-04-23 01:52:07 -0300

    Incluindo cinema

---
+++ Cinema.py
@@ -0,0 +1,10 @@
+class Cinema:
+
+    def _init_(self, id, responsavel, cnpj, endereco, telefone, cep, email):
+        self._id = id
+        self._responsavel = responsavel
+        self._cnpj = cnpj
+        self._endereco = endereco
+        self._telefone = telefone
+        self._cep = cep
+        self._email = email

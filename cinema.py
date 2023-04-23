abf018cea5ddcb7b886259f773d3acc04c3e3df6
FerCarvalhoRP <fernandacarvalhorp@gmail.com>
2023-04-23 01:30:11 -0300

    incluindo estreia

---
+++ cinema.py
@@ -0,0 +1,16 @@
+
+class Cinema:
+    def __init__(self, id, responsavel, cnpj, endereco, telefone, cep, email, ):
+        self._id = id
+        self._responsavel 
+
+
+
+
+
+
+    #class Cinema {
+    #- id: Interger- responsavel: String   - cnpj: Interger     - endereco: String    - telefone: Interger
+    #- cep: Interger     - email: String    + construtor(id, resposavel, cnpj, endereco, telefone, cep, email)
+    #+ contato(id, responsavel, telefone, email)+ cadastro completo(id, responsavel, cnpj, endereco, 
+    # telefone, cep, email)

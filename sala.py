b291257d67cbf44c0a4d009fb044f6e8d7186500
DanielaO85 <daniela.da.silva@gmail.com>
2023-04-23 19:54:37 -0300

    ajustes no codigo

--- sala.py
+++ sala.py
@@ -1,20 +1,19 @@
 class Sala:
     
-    def __init__(self, num_sala, capacidade, assentos):
+    def __init__(self, num_sala, capacidade):
         self._num_sala = num_sala
         self._capacidade = capacidade
-        self._assentos = assentos
-        assentos = [False]*30 #inicializa um array com 30 posicoes com false
+        self._assentos = [False]*30 #inicializa um array com 30 posicoes com false
        
-        def reservar_assento(self, assentos):
-         if self._assentos[assentos]:
-            return False
-         else:
-            self._assentos[assentos] = True
-            return True
+    def reservar_assento(self, assento):
+      if self._assentos[assento-1]:
+        return False
+      else:
+        self._assentos[assento-1] = True
+        return True
 
-    def buscar_asento_livre(self):
-        for i in range(len(self._assentos)):
-            if self._assentos[i] == False:
-                return i
-        return -1   
+    def buscar_assento_livre(self):
+      for i in range(len(self._assentos)):
+        if self._assentos[i] == False:
+          return i+1
+      return -1   

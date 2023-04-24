267d46db9f3d19fb8fb2f4720cf98219a5b71cdc
FerCarvalhoRP <fernandacarvalhorp@gmail.com>
2023-04-23 21:05:22 -0300

    Atualizando

--- Cinema.py
+++ Cinema.py
@@ -1,10 +1,16 @@
 class Cinema:
 
-    def _init_(self, id, responsavel, cnpj, endereco, telefone, cep, email):
+    def _init_(self, id, nome, cnpj, endereco, telefone, cep, email):
+        super().__init__(nome, endereco)
         self._id = id
-        self._responsavel = responsavel
+        self._nome = nome
         self._cnpj = cnpj
         self._endereco = endereco
         self._telefone = telefone
         self._cep = cep
         self._email = email
+    
+    def selecionar_cinema(self):
+        return self._nome._endereco
+        print ("Você selecionou o Cinema")
+    

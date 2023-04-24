9a09c9c9c7d52bc052b7ddb946f5873c81de53c4
FerCarvalhoRP <fernandacarvalhorp@gmail.com>
2023-04-23 22:50:59 -0300

    Atualização

--- estreia.py
+++ estreia.py
@@ -1,14 +1,14 @@
-from estreia import Cinema
+from cinema import Cinema
 
 class Estreia:
-    def _init_(self, id, titulo, em_cartaz, data_estreia):
-        self.id = id
+    def _init_(self, cinema, titulo, em_cartaz, data_estreia):
+        self._cinema = cinema
         self.titulo = titulo
         self.em_cartaz = em_cartaz
         self.data_estreia = data_estreia
     
-    def listar_estreia(self, Cinema, titulo, data_estreia):
-        if Cinema == self.id and titulo == self.titulo and data_estreia == self.data_estreia:
-            return f"{self.titulo} Em cartaz no Cinema: {self.id} A partir de: {self.data_estreia}"
+    def listar_estreia(self, idcinema, titulo, data_estreia):
+        if idcinema == self.cinema._id and titulo == self.titulo and data_estreia == self.data_estreia:
+            return f"{self.titulo} Em cartaz no Cinema: {self.cinema._id} {self.cinema._nome} A partir de: {self.data_estreia}"
         else:
-            return f"{self.titulo} Estreia: {Cinema} Em: {data_estreia}"
+            return f"Que tal? {self.titulo} No cinema: {Cinema} Em: {data_estreia}"
